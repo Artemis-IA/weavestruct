149663ee69d23302fd97aa8cd8310468a148befb
pi <ibrahim.mohammad@isen-ouest.yncrea.fr>
2024-12-09 12:34:19 +0100

    docs

--- src/scripts/generate_openapi.py
+++ src/scripts/generate_openapi.py
@@ -1,7 +1,13 @@
 from fastapi.openapi.utils import get_openapi
-from src.main import app
+from pathlib import Path
 import json
 import os
+import sys
+
+project_root = Path(__file__).resolve().parents[2]
+sys.path.append(str(project_root))
+
+from src.main import app
 
 # Génération du schéma OpenAPI
 def generate_openapi(output_path="docs/static/swagger.json"):
